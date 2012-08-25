46ebcf2fa0f83c70c9084c7c88a71e85ae6cc606
Christian Kindahl <christian@kindahl.com>
2012-08-25 13:14:27 +0200

    Added new string conversion functionality.

--- src/string.cc
+++ src/string.cc
@@ -17,6 +17,7 @@
  */
 
 #include <stdarg.h>
+#include <cassert>
 #include <cstring>
 #ifdef _WINDOWS
 #include <windows.h>
@@ -30,15 +31,6 @@ namespace ckcore
 {
     namespace string
     {
-        /**
-         * Wrapper around the strcmp-like functions to be compatible with
-         * different character encodings.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         int astrcmp(const tchar *str1,const tchar *str2)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -48,16 +40,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Wrapper around the strncmp-like functions to be compatible with
-         * different character encodings.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @param [in] n The number of characters to compare.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         int astrncmp(const tchar *str1,const tchar *str2,size_t n)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -67,14 +49,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Compares two strings in a case insensitive way.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         int astrcmpi(const tchar *str1,const tchar *str2)
         {
 #ifdef _WINDOWS
@@ -88,15 +62,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Compares two strings in a case insensitive way.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @param [in] n The number of characters to compare.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         int astrncmpi(const tchar *str1,const tchar *str2,size_t n)
         {
 #ifdef _WINDOWS
@@ -110,14 +75,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Wrapper around the strlen-like functions to be compatible with
-         * different character encodings.
-         * @param [in] str1 The first string.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         size_t astrlen(const tchar *str)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -127,15 +84,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Wrapper around the strcpy-like functions to be compatible with
-         * different character encodings.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         tchar *astrcpy(tchar *str1,const tchar *str2)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -145,16 +93,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Wrapper around the strncpy-like functions to be compatible with
-         * different character encodings.
-         * @param [in] str1 The first string.
-         * @param [in] str2 The second string.
-         * @param [in] n The number of characters to copy.
-         * @return If str1 is less than str2 < 0 is returned, if str1 is
-         *         identical to str2 0 is returned, if str1 is greater than
-         *         str2 > 0 is returned.
-         */
         tchar *astrncpy(tchar *str1,const tchar *str2,size_t n)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -164,13 +102,6 @@ namespace ckcore
 #endif
         }
 
-        /**
-         * Creates a formatted string from a format description similar to that
-         * of sprintf. This routine accept positional arguments like "%1$s".
-         * @param [out] res Output formatted string.
-         * @param [in] fmt The string format.
-         * @param [in] args Variable argument list.
-         */
         void vformatstr(tstring &res,const tchar * const fmt,
                         va_list args)
         {
@@ -222,12 +153,6 @@ namespace ckcore
 #endif  // #ifdef _WINDOWS
         }
 
-        /**
-         * Creates a formatted string from a format description similar to that
-         * of sprintf. This routine accept positional arguments like "%1$s".
-         * @param [in] fmt The string format.
-         * @return The resulting formatted string.
-         */
         tstring formatstr(const tchar * const fmt,...)
         {
             tstring res;
@@ -241,15 +166,6 @@ namespace ckcore
             return res;
         }
 
-        /**
-         * Converts an ANSI string into UTF-16 (big endian) format.
-         * @param [in] ansi The ANSI string to convert.
-         * @param [in] utf Pointer to buffer to which the UTF-16 string should
-         *                 be written.
-         * @param [in] utf_len The length of the utf buffer counted in UTF-16
-         *                     characters.
-         * @return The same pointer specified as the utf argument.
-         */
         wchar_t *ansi_to_utf16(const char *ansi,wchar_t *utf,int utf_len)
         {
             ckASSERT(utf_len >= 0);     // See size_t typecast below.
@@ -258,7 +174,7 @@ namespace ckcore
                                                 MB_PRECOMPOSED,ansi,(int)strlen(ansi) + 1,
                                                 utf,utf_len);
 
-            // Trunkate UTF-16 string if buffer is too small.
+            // Truncate UTF-16 string if buffer is too small.
             if (converted == utf_len)
                 utf[utf_len - 1] = '\0';
 #else
@@ -273,15 +189,6 @@ namespace ckcore
             return utf;
         }
 
-        /**
-         * Converts a UTF-16 (big endian) string into ANSI format.
-         * @param [in] utf The UTF-16 string to convert.
-         * @param [in] ansi Pointer to buffer to which the ANSI string should
-         *                  be written.
-         * @param [in] ansi_len The length of the utf buffer counted in ANSI
-         *                      characters (bytes).
-         * @return The same pointer specified as the ansi argument.
-         */
         char *utf16_to_ansi(const wchar_t *utf,char *ansi,int ansi_len)
         {
             ckASSERT(ansi_len >= 0);    // See size_t typecast below.
@@ -289,7 +196,7 @@ namespace ckcore
             int converted = WideCharToMultiByte(AreFileApisANSI() ? CP_ACP : CP_OEMCP,0,
                 utf,(int)lstrlenW(utf) + 1,ansi,ansi_len,NULL,NULL);
 
-            // Trunkate the ANSI string of buffer is too small.
+            // Truncate the ANSI string of buffer is too small.
             if (converted == ansi_len)
                 ansi[ansi_len - 1] = '\0';
 #else
@@ -304,16 +211,6 @@ namespace ckcore
             return ansi;
         }
 
-        /**
-         * Converts an ANSI string if necessary into UTF-16 format. If UTF-16 is not
-         * used the very same ANSI string is copied.
-         * @param [in] ansi The ANSI string to convert.
-         * @param [in] out Pointer to buffer to which the ANSI or UTF-16 string
-         *                 should be written.
-         * @param [in] out_len The length of the out buffer counted in UTF-16
-         *                     or ANSI characters.
-         * @return The same pointer specified as the out argument.
-         */
         tchar *ansi_to_auto(const char *ansi,tchar *out,int out_len)
         {
 #if defined(_WINDOWS) && defined(_UNICODE)
@@ -321,6 +218,36 @@ namespace ckcore
 #else
             return strncpy(out,ansi,out_len);
 #endif
+        }
+
+        tstring to_auto(const char *str)
+        {
+#if defined(_WINDOWS) && defined(_UNICODE)
+            int needed = MultiByteToWideChar(AreFileApisANSI() ? CP_ACP : CP_OEMCP,
+                                             MB_PRECOMPOSED, str, -1,
+                                             NULL, 0);
+            assert(needed > 0);
+            if (needed == 0)
+                return ckT("");
+
+            tstring res(needed - 1, ' ');
+            if (MultiByteToWideChar(AreFileApisANSI() ? CP_ACP : CP_OEMCP,
+                                    MB_PRECOMPOSED, str, -1,
+                                    const_cast<wchar_t *>(res.c_str()), needed) == 0)
+            {
+                assert(false);
+                return ckT("");
+            }
+
+            return res;
+#else
+            return str;
+#endif
+        }
+
+        tstring to_auto(const std::string &str)
+        {
+            return to_auto(str.c_str());
         }
     }
 }
